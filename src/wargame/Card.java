7127da63e08b64cba3493d5caaf4f61841ad4420
grvchh <gourav11@sheridancollege.ca>
2025-03-19 10:25:08 -0400

    Eddited Card class

--- src/wargame/Card.java
+++ src/wargame/Card.java
@@ -10,55 +10,22 @@ package wargame;
  * 
  */
 class Card {
-    private int rank; 
-    private int suit; 
-    public Card(int suit, int rank){
+    private final int rank;
+    private final int suit;
+
+    public Card(int suit, int rank) {
         this.rank = rank;
         this.suit = suit;
     }
-    public int getCard(){
-        return rank; 
-    }    
-    public void setCard(int rank){
-        this.rank = rank;
+
+    public int getRank() {
+        return rank;
     }
+
     @Override
-    public String toString(){   
-        StringBuilder displayCard = new StringBuilder();
-        switch(rank){
-            case 11:
-                displayCard.append("Jack");
-                break;
-            case 12:
-                displayCard.append("Queen");
-                break;
-            case 13:
-                displayCard.append("King");
-                break;
-            case 14:
-                displayCard.append("Ace");
-                break;    
-            default:
-                displayCard.append(rank); 
-                break;
-        }
-        displayCard.append(" of ");
-        switch(suit){
-            case 0:
-                displayCard.append("Spades");
-                break;
-            case 1:
-                displayCard.append("Hearts");
-                break;
-            case 2:
-                displayCard.append("Clubs");
-                break;
-            case 3:
-                displayCard.append("Diamonds");
-                break;
-            default: 
-                break;
-        } 
-        return displayCard.toString();
-    }  
+    public String toString() {
+        String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
+        String[] suits = {"Spades", "Hearts", "Clubs", "Diamonds"};
+        return ranks[rank - 2] + " of " + suits[suit];
+    }
 }
