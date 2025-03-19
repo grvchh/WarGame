87e5bf3c79117b16c10c9be00ecff8425ddb571d
grvchh <gourav11@sheridancollege.ca>
2025-03-19 10:49:48 -0400

    updated wargame as the instructions

--- src/wargame/WarGame.java
+++ src/wargame/WarGame.java
@@ -15,102 +15,59 @@ import java.util.Random;
  * @author Gourav,Dilpreet Singh, Simranpreet Kaur Khattra
  */
 public class WarGame {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        List<Card> cardDeck = new ArrayList<Card>(); //create an ArrayList
-        
-        for(int x=0; x<4; x++){          //0-3 for 4 suit
-            for(int y=2; y<15; y++){     //2-14 for 13  where 14 is for ace
-                cardDeck.add(new Card(x,y)); //create new card and add into the deck
-            } //end rank for
-        }//end suit for
-        
-        Collections.shuffle(cardDeck, new Random()); //shuffle the deck randomly
-        
-        //creating 2 decks, each for player1/player2
-        LinkedList<Card> deck1 = new LinkedList<Card>();
-        LinkedList<Card> deck2 = new LinkedList<Card>();
-        
-        deck1.addAll(cardDeck.subList(0, 25));                    
-        deck2.addAll(cardDeck.subList(26, cardDeck.size()));
-        
-        while(true){
-            Card p1Card = deck1.pop();  //player place one card 
-            Card p2Card = deck2.pop();
-            
-            //display the card
-            System.out.println("Player 1 plays card is " + p1Card.toString());
-            System.out.println("Player 2 plays card is " + p2Card.toString());
-            
-            //rank comparition between two cards
-            if(p1Card.getCard() > p2Card.getCard()){//if player 1 win 
-                deck1.addLast(p1Card);
-                deck1.addLast(p2Card); 
-                System.out.println("PLayer 1 wins the round");
+        List<Card> cardDeck = new ArrayList<>();
+        for (int x = 0; x < 4; x++) {
+            for (int y = 2; y <= 14; y++) {
+                cardDeck.add(new Card(x, y));
             }
- 
-            else if(p1Card.getCard() < p2Card.getCard()){//if player 2 win 
-                deck2.addLast(p1Card);   
-                deck2.addLast(p2Card);  
-                System.out.println("PLayer 2 wins the round");
-            }
-            
-            else { 
-                System.out.println("War"); 
-                
-              
-                List<Card> war1 = new ArrayList<Card>(); 
-                List<Card> war2 = new ArrayList<Card>();
-                
-                //check player have enough (4)cards
-                for(int x=0; x<3; x++){ 
-                    //one player run out of card- game over
-                    if(deck1.size() == 0 || deck2.size() == 0 ){                      
-                        break;
-                    }
-                    
-                    System.out.println("War card for player1 is xx\nWar card for player2 is xx");
+        }
+        Collections.shuffle(cardDeck, new Random());
 
-                    war1.add(deck1.pop());  
-                    war2.add(deck2.pop());                  
-                }
-                
-                //compare result 
-                if(war1.size() == 3 && war2.size() == 3 ){
-                    //display the war cards from each player
-                    System.out.println("War card for player1 is " + war1.get(0).toString());
-                    System.out.println("War card for player2 is " + war2.get(0).toString());
-                    
-                    //player 1 wins war
-                    if(war1.get(2).getCard() > war2.get(2).getCard()){
-                        deck1.addAll(war1); //player1 get 10 card
-                        deck1.addAll(war2);
-                        System.out.println("Player 1 wins the war round");
-                    }
-                    //playr 2 win war
-                    else{
-                        deck2.addAll(war1); 
-                        deck2.addAll(war2);
-                        System.out.println("Player 2 wins the war round");
-                    }                      
-                }
-                
-            }
-            
-            //Game over
-            if(deck1.size() == 0 ){
-                System.out.println("game over\nPlayer 1 wins the game");
-                break;
-            }
-            else if(deck2.size() == 0){
-                System.out.println("game over\nPlayer 2 wins the game");
-                break;
-            }
-        }  
+        Player player1 = new Player("Player 1");
+        Player player2 = new Player("Player 2");
 
+        for (int i = 0; i < 26; i++) {
+            player1.addCard(cardDeck.get(i));
+            player2.addCard(cardDeck.get(i + 26));
+        }
+
+        int round = 1;
+        while (round <= 4 && player1.getDeckSize() > 0 && player2.getDeckSize() > 0) {
+            System.out.println("Round " + round);
+            Card card1 = player1.playCard();
+            Card card2 = player2.playCard();
+
+            if (card1 == null || card2 == null) break;
+
+            System.out.println(player1.getName() + " plays " + card1);
+            System.out.println(player2.getName() + " plays " + card2);
+
+            if (card1.getRank() > card2.getRank()) {
+                player1.increaseScore();
+                System.out.println(player1.getName() + " wins this round!");
+            } else if (card1.getRank() < card2.getRank()) {
+                player2.increaseScore();
+                System.out.println(player2.getName() + " wins this round!");
+            } else {
+                System.out.println("It's a tie!");
+            }
+            round++;
+            System.out.println();
+        }
+
+        System.out.println("Final Scores: ");
+        System.out.println(player1.getName() + ": " + player1.getScore());
+        System.out.println(player2.getName() + ": " + player2.getScore());
+
+        if (player1.getScore() > player2.getScore()) {
+            System.out.println(player1.getName() + " wins the game!");
+        } else if (player1.getScore() < player2.getScore()) {
+            System.out.println(player2.getName() + " wins the game!");
+        } else {
+            System.out.println("The game is a draw!");
+        }
     }
-    }
+}
+
 
